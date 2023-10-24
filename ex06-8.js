b3b3d377de8949e9b7f060c45aaff594d4d17beb
zm22026 <131091826+zm22026@users.noreply.github.com>
2023-10-24 09:45:21 +0900

    Create ex06-8.js

---
+++ ex06-8.js
@@ -0,0 +1,3 @@
+function sayhello68() {alert('Hello, ' + document.getElementById(‘who68').innerHTML +'!');}
+function taro68() {document.getElementById(‘who68').innerHTML = '太郎';}
+function hanako68() {document.getElementById(‘who68').innerHTML = '花子';
