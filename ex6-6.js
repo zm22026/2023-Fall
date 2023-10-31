ac6ad7ffee12ea8b9296869c7fd2493b44605f53
zm22026 <131091826+zm22026@users.noreply.github.com>
2023-10-31 10:40:56 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,4 +1,5 @@
-var who66 = 'world';
-function sayhello65(){alert( who66 + 'が来た' +'!');}
+var who66 = 'が来た';
+function sayhello66(){alert( '太郎が来た' );}
+function sayhello67(){alert( '花子が来た' );}
 function taro66(){who66 = '太郎';}
 function hanako66(){who66 ='花子';}
