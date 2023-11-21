afb26572e454bc88a65343be9f102e7d5b036ec2
zm22026 <131091826+zm22026@users.noreply.github.com>
2023-11-21 10:41:39 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -1,10 +1,10 @@
 function sayhello68() {
-  alert('Hello,'+document.getElementById('who68').innerHTML + '!');
+  alert('Hello,'+ document.getElementById('who68').innerHTML + '!');
 }
 function taro68() {
   document.getElementById('who68').innerHTML = '太郎';
 }
-function hanako68(){
+function hanako68() {
   document.getElementById('who68').innerHTML = '花子';
 }
 
