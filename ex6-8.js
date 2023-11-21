def6a53228f921fa5d45e4b0ec8556f675a59b88
zm22026 <131091826+zm22026@users.noreply.github.com>
2023-11-21 10:07:09 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -2,5 +2,6 @@ function sayhello68(){alert('Hello,'+document.getElementById('who68').innerHTML+
 function taro68(){document.getElementById('who68').innerHTML='太郎';}
 function hanako68(){document.getElementById('who68').innerHTML='花子';}
 
-function gazo68()
-{document.getElementById('who68').innerHTML='<img src="./image/bulbon.gif" alt="オン">’;}
+function gazo688()
+{document.getElementById('who688').innerHTML=
+  '<img src="./image/bulbon.gif" alt="オン">’;}
