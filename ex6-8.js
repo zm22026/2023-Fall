384f3df777332837934db9e75066c87345bceac4
zm22026 <131091826+zm22026@users.noreply.github.com>
2023-12-05 09:43:13 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -1,15 +1,9 @@
-function sayhello68() {
+function sayhello68(){
   alert('Hello,'+ document.getElementById('who68').innerHTML + '!');
 }
-function taro68() {
+function taro68(){
   document.getElementById('who68').innerHTML = '太郎';
 }
-function hanako68() {
+function hanako68(){
   document.getElementById('who68').innerHTML = '花子';
 }
-
-
-function gazo688()
-{document.getElementById('who688').innerHTML=
-  '<img src="./image/bakuhatsu.png" alt="オン">’
-  ;}
