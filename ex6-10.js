5e5b813fc1aa9dc799390e00aae22d9bedc5c8a1
zm22026 <131091826+zm22026@users.noreply.github.com>
2024-01-07 11:59:26 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,4 +1,4 @@
 function imgchange1() {
 document.getElementById('logo').src ='image/bakudan.png';
 function imgchange2() {
-document.getElementById('logo').src ='image/bakuhatsu.png'（画像名）;
+document.getElementById('logo').src ='image/bakuhatsu.png';
